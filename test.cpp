e2ab021b2c0c1afcb13a6ff9a88105f834c4ee2d
Refazul Refat <refazul.refat@gmail.com>
2015-01-03 17:20:11 +0600

    Update

--- test.cpp
+++ test.cpp
@@ -3,18 +3,26 @@
 #include <stdio.h>
 using namespace std;
 
+#define DIM 2
+
 class Utility{
 	
 private:
 	int dim;
 	int* memory;
 public:
+	Utility();
 	Utility(int);
 	Utility(int,int*);
 	void setUtility(int*);
 	void printUtility();
 	int get(int,int);
 };
+Utility::Utility(){
+	
+	dim=DIM;
+	memory=new int[dim*dim];
+}
 Utility::Utility(int d){
 	
 	dim=d;
@@ -50,6 +58,31 @@ int Utility::get(int x,int y){
 	return memory[x*dim+y];
 }
 
+class UtilitySet{
+	
+private:
+	int total;
+	Utility* u;
+public:
+	UtilitySet(int);
+	void setUtility(int,Utility*);
+	Utility* getUtility(int);
+};
+
+UtilitySet::UtilitySet(int t){
+	
+	total=t;
+	u=new Utility[total];
+}
+void UtilitySet::setUtility(int index,Utility* util){
+	
+	u[index]=*util;
+}
+Utility* UtilitySet::getUtility(int index){
+	
+	return &u[index];
+}
+
 int main(){
 	
 	int t[]={1,3,2,1};
@@ -57,6 +90,12 @@ int main(){
 	u->setUtility(&t[0]);
 	u->printUtility();
 	cout<<u->get(0,1)<<endl;
+	
+	UtilitySet* set=new UtilitySet(3);
+	set->setUtility(0,u);
+	
+	Utility* ut=set->getUtility(0);
+	ut->printUtility();
 	
 	return 0;
 }
